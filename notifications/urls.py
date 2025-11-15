1b1b1118436bf96ab06afa81643d0ca0b25cee56
Elkana Maina <elisoft.engineer@gmail.com>
2025-11-15 11:39:35 +0300

    Add notifications urls

---
+++ notifications/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.NotificationList.as_view()),
+    path('<uuid:pk>/', views.NotificationDetail.as_view()),
+]
