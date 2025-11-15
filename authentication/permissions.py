039c44d4ce6d012b0640317e4319dd0c45c88822
Elkana Maina <elisoft.engineer@gmail.com>
2025-11-15 11:35:05 +0300

    Add Staff and Owner permissions

---
+++ authentication/permissions.py
@@ -0,0 +1,33 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsStaff(BasePermission):
+    """
+    Allows access only to admin users.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_staff
+    
+
+class IsOwner(BasePermission):
+    """
+    Allow access to user relations such as notifications.
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.user.id == request.user.id
+    
+
+class IsSelfOrStaff(BasePermission):
+    """
+    Custom permission to allow access to the user's details
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.email == request.user.email or request.user.is_staff
+    
+
+class IsSeller(BasePermission):
+    """
+    Regulate modifications and deletions on objects
+    """
+    def has_object_permission(self, request, view, obj):
+        return obj.seller.id == request.user.id
